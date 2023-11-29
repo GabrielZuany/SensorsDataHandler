0c96f1723a19aa6aed5006e20822da0a72508ea6
GabrielZuany <gzuanydev@gmail.com>
2023-11-29 02:29:25 -0300

    Update setup.sh

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -3,12 +3,12 @@ NC='\033[0m' # No Color
 BLUE='\033[0;34m'
 
 echo -e "${GREEN}|=======================|Downloading Kafka|=======================|${NC}"
-# wget https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz
+wget https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz
 
 echo -e "${GREEN}|=======================|Extracting Kafka|=======================|${NC}"
-# tar -xzf kafka_2.12-2.8.0.tgz
-# mv kafka_2.12-2.8.0 kafka
-# rm kafka_2.12-2.8.0.tgz
+tar -xzf kafka_2.12-2.8.0.tgz
+mv kafka_2.12-2.8.0 kafka
+rm kafka_2.12-2.8.0.tgz
 
 echo -e "${GREEN}|=======================|Starting Zookeeper|=======================|${NC}"
 gnome-terminal -- ./kafka/kafka_2.12-2.8.0/bin/zookeeper-server-start.sh ./kafka/kafka_2.12-2.8.0/config/zookeeper.properties
