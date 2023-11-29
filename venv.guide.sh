025e8b3cc634d396887308970bfe5b96ce55eaf9
GabrielZuany <gzuanydev@gmail.com>
2023-11-29 19:36:45 -0300

    Update venv.guide.sh

--- venv.guide.sh
+++ venv.guide.sh
@@ -1,2 +1,3 @@
 echo -e "${GREEN}|=======================|Setting up Virtual Environment|=======================|${NC}"
-python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+python3 -m venv venv && source venv/bin/activate 
+pip install -r requirements.txt
